5a3a8561bab99d340f4cd4461746b17290011cc7
Leonid <leonid.ihnatenko@exxeta.com>
2018-06-07 11:48:50 +0200

    RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +12,27 @@ namespace GradeBook.GradeBooks
 	    public RankedGradeBook(string name) : base(name)
 	    {
 		    Type = GradeBookType.Ranked;
+	    }
+
+	    public override char GetLetterGrade(double averageGrade)
+	    {
+			if (base.Students.Count < 5)
+				throw new InvalidOperationException("Ranked grading required 5 or more students");
+
+		    var treshhold = (int)Math.Ceiling(Students.Count * 0.2);
+
+		    var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+		    if (grades[treshhold - 1] <= averageGrade)
+			    return 'A';
+			else if (grades[(treshhold*2)-1] <= averageGrade)
+				return 'B';
+		    else if (grades[(treshhold * 3) - 1] <= averageGrade)
+			    return 'C';
+		    else if (grades[(treshhold * 4) - 1] <= averageGrade)
+			    return 'D';
+			else
+			return 'F';
 	    }
     }
 }
